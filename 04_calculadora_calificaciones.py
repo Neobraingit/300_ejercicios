84d32464583632aee546f08ded71e65e3bc4086b
neobraingit <neobrain@protonmail.com>
2024-11-14 18:33:43 +0100

    Calificaciones

---
+++ 04_calculadora_calificaciones.py
@@ -0,0 +1,32 @@
+
+''' Un programa que permita al usuario introducir calificaciones de alumnos y calcular el promedio'''
+from colorama import Fore, Back, Style
+from tqdm import tqdm
+import time
+
+def calcular_promedio(calificaciones):
+    total = sum(calificaciones)
+    promedio = total/len(calificaciones)
+    return promedio
+
+
+calificaciones = []
+
+while True:
+    nota = float(input('Introduce una nota(-1 para terminar): '))
+    if nota == -1:
+        break
+    calificaciones.append(nota)
+    
+    if calificaciones:
+        promedio = calcular_promedio(calificaciones)
+        print (Fore.RED +'El promedio de calificaciones es: ' + Style.RESET_ALL, promedio)
+        time.sleep(2)
+    else:
+        print (Fore.BLUE + 'No se introdujeron notas' + Style.RESET_ALL)
+    
+
+
+
+    
+    
